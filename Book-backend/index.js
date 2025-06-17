26e2d63933ea9d32c1718fe4bb80067d1fb2629f
Jyothikalajoo <jothikalavenkatachalam@gmail.com>
2025-06-17 12:24:07 +0530

    Update index.js

--- Book-backend/index.js
+++ Book-backend/index.js
@@ -17,3 +17,7 @@ app.use('/api/orders', require('./routes/orderRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = (req, res) => {
+  res.status(200).send('Hello from Vercel Serverless!');
+};
